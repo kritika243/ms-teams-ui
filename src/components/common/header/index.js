9597a845c4fd75dbc07824d30d57f92064562e0d
Kritika <24kritikasharma24@gmail.com>
2021-07-15 23:40:18 +0530

    header component built and styled

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import './header.css'
 
 function Header() {
-  return <div>This is header</div>
+  return (
+    <div className='header'>
+      <div className='header-menu'>
+        <i class='fi-rr-layout-fluid'></i>
+      </div>
+      <div className='header-leftMain'>
+        <label className='header-label'>Microsoft Teams</label>
+      </div>
+      <div className='header-rightMain'>
+        <div className='header-search'>
+          <i class='fi-rr-search'></i>
+          <input type='text' placeholder='Search' />
+        </div>
+        <div className='header-profile'>
+          <div className='header-options'>
+            <i class='fi-rr-menu-dots'></i>
+          </div>
+          <img
+            className='header-avatar'
+            src='https://image.flaticon.com/icons/png/128/145/145843.png'
+            alt='avatar'
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
