e0276e24e25cd5b26054e2322b5b1d276f77cc25
Kritika <24kritikasharma24@gmail.com>
2021-07-16 21:29:12 +0530

    :white_check_mark:sidebar component

--- src/components/calls/left-main/index.js
+++ src/components/calls/left-main/index.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import { CallsMenu, DialerList } from '../../../data/calls'
+import DialerItem from './dialer-item'
+import './left-main.css'
+import MenuItem from './menu-item'
 
 function LeftMain() {
-  return <div>This is left main</div>
+  const menu = CallsMenu
+  const dialerList = DialerList
+  return (
+    <div className='leftMain'>
+      <div className='leftMain-menu'>
+        <label className='menu-label'>Calls</label>
+        <div className='menu-items'>
+          {menu.map((item) => {
+            return <MenuItem item={item} />
+          })}
+        </div>
+      </div>
+      <div className='leftMain-dialer'>
+        <label className='dialer-label'>Make a call</label>
+        <div className='dialer-search'>
+          <input type='text' placeholder='Type a name' />
+        </div>
+        <div className='dialer-suggested'>
+          <label className='suggested-label'>Suggested</label>
+          <div className='suggested-list'>
+            {dialerList.map((item) => {
+              return <DialerItem item={item} />
+            })}
+          </div>
+        </div>
+        <div className='dialer-botton'>
+          <div className='dialer-bottom-icon'>
+            <i className='fi-rr-microphone ic'></i>
+          </div>
+          <div className='dialer-bottom-icon'>
+            <i className='fi-rr-video-camera ic'></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LeftMain
---
+++ src/data/calls.js
@@ -0,0 +1,43 @@
+export const CallsMenu = [
+  {
+    id: 1,
+    name: 'Speed Dial',
+    icon: <i class='fi-rr-smartphone'></i>,
+  },
+  {
+    id: 2,
+    name: 'Contacts',
+    icon: <i class='fi-rr-portrait'></i>,
+  },
+  {
+    id: 3,
+    name: 'History',
+    icon: <i class='fi-rr-time-past'></i>,
+  },
+  {
+    id: 4,
+    name: 'Voicemail',
+    icon: <i class='fi-rr-microphone'></i>,
+  },
+]
+
+export const DialerList = [
+  {
+    id: 1,
+    name: 'Camilla Carline',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/949/949635.png',
+  },
+  {
+    id: 2,
+    name: 'Mark Howard',
+    type: 'Teacher',
+    avatar: 'https://image.flaticon.com/icons/png/128/921/921071.png',
+  },
+  {
+    id: 3,
+    name: 'Patrick Perry',
+    type: 'Admin',
+    avatar: 'https://image.flaticon.com/icons/png/128/3048/3048122.png',
+  },
+]
