3cdce17bed84efc6cd92d1e7b1dda8dc2d1523ab
Kritika <24kritikasharma24@gmail.com>
2021-07-16 18:41:27 +0530

    sidebar component:white_check_mark:

--- src/components/common/sidebar/index.js
+++ src/components/common/sidebar/index.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { SidebarBottom, SidebarData, SidebarMore } from '../../../data/sidebar'
+import SidebarOption from './sidebar-option'
+import './sidebar.css'
 
 function Sidebar() {
-  return <div>This is sidebar</div>
+  const topOptions = SidebarData
+  const more = SidebarMore
+  const bottomOptions = SidebarBottom
+  return (
+    <div className='sidebar'>
+      <div className='sidebar-top'>
+        <div>
+          {topOptions.map((option) => {
+            return (
+              <SidebarOption
+                option={option}
+                isActive={option.name === 'Calls' ? true : false}
+              />
+            )
+          })}
+        </div>
+        <div>
+          <div>
+            <SidebarOption option={more} />
+          </div>
+        </div>
+      </div>
+      <div className='sidebar-bottom'>
+        {bottomOptions.map((option) => {
+          return <SidebarOption option={option} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Sidebar
