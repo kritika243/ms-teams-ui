f2cd911c34b834ee2c8c5d48367ad6475a8869b5
Kritika <24kritikasharma24@gmail.com>
2021-07-16 23:10:59 +0530

    main contact cards component:white_check_mark:

--- src/components/calls/right-main/index.js
+++ src/components/calls/right-main/index.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { ContactCards } from '../../../data/calls'
+import ContactCard from './contact-card'
+import './right-main.css'
 
 function RightMain() {
-  return <div>This is Right main</div>
+  const contacts = ContactCards
+  return (
+    <div className='rightMain'>
+      <div className='rightMain-heading'>
+        <label className='heading-label'>Contacts</label>
+      </div>
+      <div className='rightMain-options'>
+        <div className='contact-search'>
+          <input placeholder='Find a contact' />
+          <div className='contact-search-icon'>
+            <i class='fi-rr-search'></i>
+          </div>
+        </div>
+        <div className='add-button'>
+          <div className='add-icon'>
+            <i class='fi-rr-user-add'></i>
+          </div>
+          <label className='add-label'>Add Contact</label>
+        </div>
+      </div>
+      <div className='contact-list'>
+        {contacts.map((item) => {
+          return <ContactCard item={item} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default RightMain
--- src/data/calls.js
+++ src/data/calls.js
@@ -41,3 +41,54 @@ export const DialerList = [
     avatar: 'https://image.flaticon.com/icons/png/128/3048/3048122.png',
   },
 ]
+
+export const ContactCards = [
+  {
+    id: 1,
+    name: 'Fabian Steele',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/4333/4333609.png',
+  },
+  {
+    id: 2,
+    name: ' Donald Mason',
+    type: 'Teacher',
+    avatar: 'https://image.flaticon.com/icons/png/128/145/145867.png',
+  },
+  {
+    id: 3,
+    name: 'Grace Stevens',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/3667/3667325.png',
+  },
+  {
+    id: 4,
+    name: 'Lesley Ford',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/145/145852.png',
+  },
+  {
+    id: 5,
+    name: 'Hayden Gildon',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/921/921110.png',
+  },
+  {
+    id: 6,
+    name: 'Sadie Matthews',
+    type: 'Teacher',
+    avatar: 'https://image.flaticon.com/icons/png/128/921/921124.png',
+  },
+  {
+    id: 7,
+    name: 'Russ Morales',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/3048/3048189.png',
+  },
+  {
+    id: 8,
+    name: 'Basil Bailey',
+    type: 'Student',
+    avatar: 'https://image.flaticon.com/icons/png/128/921/921089.png',
+  },
+]
